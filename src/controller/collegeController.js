d751a9cbc268df316c3aede5130a5b84850fec52
pattamu <102967813+pattamu@users.noreply.github.com>
2022-05-05 08:06:05 +0530

    validateurl function added

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -10,39 +10,41 @@ function validatefullName(fullname) {
     return fullNamePattern.test(fullname.trim());
 }
 
+function validateURL(url) {
+    var urlPattern = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/gm
+    return urlPattern.test(url.trim());
+}
+
 const createCollege = async function (req, res) {
     try {
         let data = req.body
-        let urlRegEx = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/gm
         
-        if (Object.keys(data).length != 0) {
+        if (Object.keys(data).length == 0)
+            return res.status(400).send({status: false, msg: "Please Enter college details to create college Data." })
 
-            if (!data.name) 
-                return res.status(400).send({ status: false, msg: "plz enter name in short form" })
-            
-            if (!validateName(data.name)) 
-                return res.status(400).send({ status: false, msg: "plz enter valid short name of college" })
-            
-            if (!data.fullName) 
-                return res.status(400).send({ status: false, msg: "plz enter fullName of the college" })
-            
-            if (!validatefullName(data.fullName)) 
-                return res.status(400).send({ status: false, msg: "plz enter valid fullName of the college" })
-            
-            data.fullName = data.fullName.split(' ').map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase()).join(' ')
+        if (!data.name) 
+            return res.status(400).send({ status: false, msg: "plz enter name in short form" })
+        
+        if (!validateName(data.name)) 
+            return res.status(400).send({ status: false, msg: "plz enter valid short name of college" })
+        
+        if (!data.fullName) 
+            return res.status(400).send({ status: false, msg: "plz enter fullName of the college" })
+        
+        if (!validatefullName(data.fullName)) 
+            return res.status(400).send({ status: false, msg: "plz enter valid fullName of the college" })
+        
+        data.fullName = data.fullName.split(' ').map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase()).join(' ')
 
-            if(data.logoLink && !urlRegEx.test(data.logoLink.trim()))
-                return res.status(400).send({ status: false, msg: "Please Enter a valid URL for the logoLink."})
-                
-            let a = await collegeModel.find({ name: data.name })
-            if (a.length != 0) 
-                return res.status(400).send({ status: false, msg: "This Name is already used." })
-                
-            let savedData = await collegeModel.create(data)
-                res.status(201).send({status: true, data: savedData })
-        }
-        else 
-            res.status(400).send({status: false, msg: "Please Enter college details to create college Data." })
+        if(data.logoLink && !validateURL(data.logoLink))
+            return res.status(400).send({ status: false, msg: "Please Enter a valid URL for the logoLink."})
+            
+        let a = await collegeModel.find({ name: data.name })
+        if (a.length != 0) 
+            return res.status(400).send({ status: false, msg: "This Name is already used." })
+            
+        let savedData = await collegeModel.create(data)
+            res.status(201).send({status: true, data: savedData })
     }
     catch (error) {
         console.log(error.message)
